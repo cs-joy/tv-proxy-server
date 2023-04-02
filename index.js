94e62e72e5584555cbf9b5d5839cb66a76b59fae
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-04-02 23:47:52 +0600

    err: ECONNRESET // new to debug

--- index.js
+++ index.js
@@ -7,26 +7,26 @@ import cors from "cors";
 
 // tulind function
 // const {sma_inc} = require("./indicators.js");
-// import {sma_inc} from "./indicators.js";
+//  import sma_inc from "./indicators.js";
 
 import tulind from "tulind";
 import {promisify} from "util";
 
 const sma_async = promisify(tulind.indicators.sma.indicator);
 
-const sma_inc = async () => {
+const sma_inc = async (data) => {
    const d1 = data.map((d) => d.close);
    const results = await sma_async([d1], [100]);
    const d2 = results[0];
    const diff = data.length - d2.length;
-   const emptyArr = [...new Array(diff)].map(d => '');
+   const emptyArr = [...new Array(diff)].map((d) => '');
    const d3 = [...emptyArr, ...d2];
    data = data.map((d, i) => ({ ...d, sma: d3[i] }));
 
    return data;
 };
 
-app.use(cors())
+app.use(cors());
 app.get('/:symbol/:interval', async (req, res) => {
     try {
     const {symbol, interval} = req.params;
@@ -34,7 +34,7 @@ app.get('/:symbol/:interval', async (req, res) => {
         `https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=${interval}`
     );
     const data = JSON.parse(resp.body);
-    var klinedata = data.map((d) => ({
+    let klinedata = data.map((d) => ({
       time: d[0] / 1000,
       open: d[1] * 1,
       high: d[2] * 1,
--- indicators.js
+++ indicators.js
@@ -5,15 +5,15 @@ import {promisify} from "util";
 
 const sma_async = promisify(tulind.indicators.sma.indicator);
 
-const sma_inc = async () => {
+const sma_inc = async (data) => {
    const d1 = data.map((d) => d.close);
    const results = await sma_async([d1], [100]);
    const d2 = results[0];
    const diff = data.length - d2.length;
-   const emptyArr = [...new Array(diff)].map(d => '');
+   const emptyArr = [...new Array(diff)].map((d) => '');
    const d3 = [...emptyArr, ...d2];
    data = data.map((d, i) => ({ ...d, sma: d3[i] }));
-
+   
    return data;
 };
 // export default sma_inc;
