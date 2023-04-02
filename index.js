316dc9c7d0650cbbaaf27089ea93527b6e3649b7
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-04-02 19:18:27 +0600

    v2 test

--- index.js
+++ index.js
@@ -5,6 +5,27 @@ const server = app.listen(3232, log('Proxy server is running at port 3232'));
 import got from "got";
 import cors from "cors";
 
+// tulind function
+// const {sma_inc} = require("./indicators.js");
+// import {sma_inc} from "./indicators.js";
+
+import tulind from "tulind";
+import {promisify} from "util";
+
+const sma_async = promisify(tulind.indicators.sma.indicator);
+
+const sma_inc = async () => {
+   const d1 = data.map((d) => d.close);
+   const results = await sma_async([d1], [100]);
+   const d2 = results[0];
+   const diff = data.length - d2.length;
+   const emptyArr = [...new Array(diff)].map(d => '');
+   const d3 = [...emptyArr, ...d2];
+   data = data.map((d, i) => ({ ...d, sma: d3[i] }));
+
+   return data;
+};
+
 app.use(cors())
 app.get('/:symbol/:interval', async (req, res) => {
     try {
@@ -13,13 +34,14 @@ app.get('/:symbol/:interval', async (req, res) => {
         `https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=${interval}`
     );
     const data = JSON.parse(resp.body);
-    const klinedata = data.map((d) => ({
+    var klinedata = data.map((d) => ({
       time: d[0] / 1000,
       open: d[1] * 1,
       high: d[2] * 1,
       low: d[3] * 1,
       close: d[4] * 1,
     }));
+    klinedata = await sma_inc(klinedata);
     res.status(200).json(klinedata);
   } catch (err) {
     res.status(500).send(err);
---
+++ indicators.js
@@ -0,0 +1,20 @@
+const {log, error} = console;
+
+import tulind from "tulind";
+import {promisify} from "util";
+
+const sma_async = promisify(tulind.indicators.sma.indicator);
+
+const sma_inc = async () => {
+   const d1 = data.map((d) => d.close);
+   const results = await sma_async([d1], [100]);
+   const d2 = results[0];
+   const diff = data.length - d2.length;
+   const emptyArr = [...new Array(diff)].map(d => '');
+   const d3 = [...emptyArr, ...d2];
+   data = data.map((d, i) => ({ ...d, sma: d3[i] }));
+
+   return data;
+};
+// export default sma_inc;
+module.exports = { sma_inc };
